523a282fcd0db7df507053565fbf8904c8b53ee7
ososa <octavio.sosa.f@gmail.com>
2019-04-26 17:25:30 -0500

    def main()

--- visuals.py
+++ visuals.py
@@ -14,16 +14,21 @@ class OneDim:
         bud1 = buds[0]
         bud2 = buds[1]
         bud3 = buds[2]
-        points = np.concatenate(me.values, buds)
 
         colors = [me.color, bud1.color, bud2.color, bud3.color]
-        plt.quiver(*origin, me.values, bud1.values, bud2.values, bud3.values, color = colors, scale=21)
+        plt.quiver(*origin, me.values, bud1.values, bud2.values, bud3.values, color=colors, scale=21)
 
         plt.show()
 
 
 
 def main():
+    me = Tensor(1, [2, 2], 'red')
+    bud1 = Tensor(1, [3, -5], 'green')
+    bud2 = Tensor(1, [1, 3], 'blue')
+    bud3 = Tensor(1, [1, 4], 'brown')
+    buds = [bud1, bud2, bud3]
+
     one_D = OneDim(me, buds)
 
 main()
