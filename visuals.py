3dd081ebfa157c7d3d0a5755494a1325d1fa7ad8
ososa <octavio.sosa.f@gmail.com>
2019-04-26 17:40:38 -0500

    plot vectors

--- visuals.py
+++ visuals.py
@@ -9,14 +9,21 @@ class OneDim:
         # set figure
         fig = plt.figure()
         ax = fig.add_subplot(111)
+        plt.xlim(-8, 8)
+        plt.ylim(-8, 8)
+        plt.grid(True)
 
         origin = [0], [0]
         bud1 = buds[0]
         bud2 = buds[1]
         bud3 = buds[2]
 
+        V = np.array([me.values, bud1.values, bud2.values, bud3.values])
+
         colors = [me.color, bud1.color, bud2.color, bud3.color]
-        plt.quiver(*origin, me.values, bud1.values, bud2.values, bud3.values, color=colors, scale=21)
+        print(V)
+
+        plt.quiver(*origin, V[:, 0], V[:, 1], color=colors, angles='xy', scale_units='xy', scale=1)
 
         plt.show()
 
@@ -26,7 +33,7 @@ def main():
     me = Tensor(1, [2, 2], 'red')
     bud1 = Tensor(1, [3, -5], 'green')
     bud2 = Tensor(1, [1, 3], 'blue')
-    bud3 = Tensor(1, [1, 4], 'brown')
+    bud3 = Tensor(1, [-1, -4], 'magenta')
     buds = [bud1, bud2, bud3]
 
     one_D = OneDim(me, buds)
